52b2d85173ba632c4094672207e9f38cbfa3e988
Mr. Baguetter <70399915+Mr-Baguetter@users.noreply.github.com>
2024-08-08 19:23:20 -0500

    Small Change

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -1,4 +1,5 @@
 ﻿using Exiled.API.Features;
+using Exiled.API.Features.Roles;
 using Exiled.Events.EventArgs.Map;
 using Exiled.Events.EventArgs.Server;
 using Exiled.Loader;
@@ -7,6 +8,9 @@ using PlayerRoles;
 using Respawning;
 using System;
 using System.Collections.Generic;
+using Exiled.API.Features.Hazards;
+using RelativePositioning;
+using UnityEngine;
 
 namespace Tank173
 {
@@ -19,5 +23,19 @@ namespace Tank173
 
           public void OnRoundStarted() => Tank173.Instance.IsSpawnable = true;
           public float SpawnChance { get; set; } = 100;
+     }
+
+     internal class Program
+     {
+          public static void Main(string[] args)
+          {
+               TantrumHazard PlaceTantrum(bool failIfObserved = false, float cooldown = 45)
+               {
+                    if() public Vector3 Position { get; set; } = public RelativePosition SynchronisedPosition { get; set; }
+
+
+          
+               }
+          }
      }
 }
